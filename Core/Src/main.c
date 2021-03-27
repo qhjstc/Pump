3b3ed8fce21fbca7f7c06f6c3a6e8b0dbc7ca24c
qhjstc <654390596@qq.com>
2021-03-27 19:03:08 +0800

    llalal

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -96,7 +96,7 @@ int main(void)
   MX_USART2_UART_Init();
   MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
-  HAL_Delay(500);
+  HAL_Delay(200);
   printf("This is Pump project!\r\n");
   Steer_Init();
   Steer_Start();
